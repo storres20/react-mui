daa5947f69877ba845b2061489c242bd2c26484a
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-18 12:40:51 -0500

    Add Mui - Toggle Button

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { Stack, Button, IconButton, ButtonGroup } from '@mui/material'
+import React, {useState} from 'react'
+import { Stack, Button, IconButton, ButtonGroup, ToggleButtonGroup, ToggleButton } from '@mui/material'
 import SendIcon from '@mui/icons-material/Send';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
 
 export const MuiButton = () => {
+  const [formats, setFormats] = useState<string | null>(null)
+  console.log({formats});
+  
+  const handleFormatChange = (_event: React.MouseEvent<HTMLElement>, updatedFormats: string | null) => {
+    setFormats(updatedFormats)
+  }
+  
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction='row'>
@@ -40,6 +50,14 @@ export const MuiButton = () => {
         <Button>Center</Button>
         <Button>Right</Button>
       </ButtonGroup>
+      </Stack>
+      
+      <Stack direction='row'>
+        <ToggleButtonGroup aria-label='text-formatting' value={formats} onChange={handleFormatChange} size='small' color='success' orientation='vertical' exclusive>
+          <ToggleButton value='bold' aria-label='bold'><FormatBoldIcon/></ToggleButton>
+          <ToggleButton value='italic' aria-label='italic'><FormatItalicIcon/></ToggleButton>
+          <ToggleButton value='underlined' aria-label='underlined'><FormatUnderlinedIcon/></ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
   )
